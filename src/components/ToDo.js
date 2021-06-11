16d20168646ac966d3869e8dd6ab44b4df0d8c69
Naren <narayanan@welcometocbs.com>
2021-06-11 11:00:23 -0400

    added icons packages

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,15 @@
 import React, {useState}  from 'react'
+import ToDoList from './ToDoList'
 
 function Todo() {
-    const [state, setstate] = useState(initialState)
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    })
 
-    return (
-        <div>
-            
-        </div>
-    )
+    return todos.map((todo, index) => (
+        <div className = {todo.isFinished ? 'todo-row finished' : 'todo-row'} key={index} ></div>
+    ))
 }
 
 export default Todo
